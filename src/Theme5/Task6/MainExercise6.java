738ca6c2d87def887c6449130677cc20add63c3b
Nazarii Yaremchuk <nnazar257@gmail.com>
2019-01-25 15:58:21 +0200

    Problem 6 has been fixed. Added comment

--- src/Theme5/Task6/MainExercise6.java
+++ src/Theme5/Task6/MainExercise6.java
@@ -1,12 +1,15 @@
 package Theme5.Task6;
 
+/**
+ * Переробіть попереднє завдання так , щоб два перегружених методи приймали два аргументи(різних типів).
+ * і відрізнялись тільки порядком їх слідування в списку аргументів. Перевірте,чи працює це
+ */
+
 public class MainExercise6 {
     public static void main(String[] args) {
         Car car = new Car();
-
-
-        car.drive('A',2);
-        car.drive(4,'B');
-        car.drive(60,1.5);
+        car.drive('A', 2);
+        car.drive(4, 'B');
+        car.drive(60, 1.5);
     }
 }
