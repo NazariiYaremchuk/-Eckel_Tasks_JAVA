9e01a2bcad95f766b933de1e26546b9770c75b11
Nazarii Yaremchuk <nnazar257@gmail.com>
2019-01-25 16:00:50 +0200

    Problem 7 has been fixed. Added comment

--- src/Theme5/Task7/MainExercise7.java
+++ src/Theme5/Task7/MainExercise7.java
@@ -1,5 +1,10 @@
 package Theme5.Task7;
 
+/**
+ * Створіть клас без конструктору. Створіть об’єкт цього класу в методі main(),
+ * щоб перевірити , що конструктор за замовчуванням синтезується автоматично. *
+ */
+
 public class MainExercise7 {
     public static void main(String[] args) {
         ClassTask7 classTask7 = new ClassTask7();
