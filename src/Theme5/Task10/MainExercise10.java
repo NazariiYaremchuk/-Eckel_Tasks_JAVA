b5c9882e1d91c8e1d476bee0c32256e5d058b39b
Nazarii Yaremchuk <nnazar257@gmail.com>
2019-01-25 13:37:35 +0200

    I fixed 10 tasks. Got a comment from the project

--- src/Theme5/Task10/MainExercise10.java
+++ src/Theme5/Task10/MainExercise10.java
@@ -3,6 +3,5 @@ package Theme5.Task10;
 public class MainExercise10 {
     public static void main(String[] args) {
         HelpClassToExercise10 variable = new HelpClassToExercise10();
-//        variable.finalize();
     }
 }
