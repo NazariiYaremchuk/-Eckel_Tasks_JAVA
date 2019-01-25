8351afd0269e306d629bd20197d72fcf997044f6
Nazarii Yaremchuk <nnazar257@gmail.com>
2019-01-25 15:49:19 +0200

    Problem 17 has been fixed. Added comment

--- src/Theme5/Task3/MainExercise3.java
+++ src/Theme5/Task3/MainExercise3.java
@@ -1,5 +1,9 @@
 package Theme5.Task3;
 
+/**
+ * Створити клас з дефолтним конструктором (без параметрів) ,який виводить на екран повідомлення.
+ * Створіть об’єкт цього класу
+ */
 public class MainExercise3 {
     public static void main(String[] args) {
         AuxiliaryClass auxiliaryClass = new AuxiliaryClass();
