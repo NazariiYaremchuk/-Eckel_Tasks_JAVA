8916705a7509e0a13dfb12a9b2792e781022ab41
Nazarii Yaremchuk <nnazar257@gmail.com>
2019-01-25 15:52:15 +0200

    Problem 4 has been fixed. Added comment

--- src/Theme5/Task4/MainExercise4.java
+++ src/Theme5/Task4/MainExercise4.java
@@ -1,5 +1,10 @@
 package Theme5.Task4;
 
+/**
+ * Добавте до класу із вправи 3 перегружений конструктор, приймаючий в якості параметрів рядок(String),
+ * і роздруковуючий його разом з повідомленням
+ */
+
 public class MainExercise4 {
     public static void main(String[] args) {
         new AuxiliaryClass("QWERTY");
